15b8d805f18f956ffc5de2bd2b96386a9998dd46
Aayush Rijal <arijal@aiims.com.au>
2024-02-08 10:41:39 +1100

    WIP: responsive design

--- page-templates/inner-service.php
+++ page-templates/inner-service.php
@@ -10,8 +10,54 @@ get_template_part('parts/section', 'banner');
 
 <main class="innerservicePage">
     <div class="container">
-        
+        <article class="fs-27 fw-600 text-tertiary text-center description py-7">
+            <p>Demolition is not merely about bringing structures down; it's a symphony of precision, safety, and expertise. When it comes to choosing a partner for demolition services, one name stands out amidst the crowd: Dhilas Excavation & Demolition. With a track record that echoes excellence and a commitment to innovation, Dhilas is your ultimate choice for transforming spaces with a touch of mastery.</p>
+        </article>
+
+        <div class="row">
+            <div class="col-8">
+                <div class="row g-5">
+                    <div class="col-12">
+                        <article class="bg-primary-light py-4 px-6 rounded-20 shadow">
+                            <p class="col-lg-7 text-tertiary fs-35 fw-700">
+                                Why choose Dhilas for your Demolition needs?
+                            </p>
+                        </article>
+                    </div>
+
+                    <div class="col-5">
+                        <div class="bg-primary rounded-20 shadow">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="row g-5">
+                    <div class="bg-primary rounded-20 shadow">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
+    <?php get_template_part('parts/section', 'quickquote'); ?>
+
+    <div class="pt-7">
+        <?php get_template_part('parts/section', 'services'); ?>
+    </div>
+
+    <?php get_template_part('parts/section', 'ournumbers'); ?>
+    <?php get_template_part('parts/section', 'priority'); ?>
+
+    <div class="pt-7">
+        <?php get_template_part('parts/section', 'testimonials'); ?>
+    </div>
+
+    <?php get_template_part('parts/section', 'ourclient'); ?>
+    <?php get_template_part('parts/section', 'commonform'); ?>
 </main>
 
 <?php get_footer(); ?>
--- parts/section-about.php
+++ parts/section-about.php
@@ -36,10 +36,25 @@
                 </div>
             </div>
 
-            <div class="col-6 position-relative">
-                <div class="position-absolute bottom-0 w-100 bg-primary h-50 opacity-50 rounded-20"></div>
-                <div class="video bg-black h-100">
+            <div class="col-6">
+                <div class="video-banner">
+                    <div class="backdrop position-absolute bottom-0 w-100 bg-primary opacity-50 start-0 rounded-20"></div>
+                    
+                    <svg class="vector-back" xmlns="http://www.w3.org/2000/svg" width="65" height="80" viewBox="0 0 65 80" fill="none">
+                        <path d="M0 80C14.244 59.9058 21.5215 37.237 25.8134 13.5636C26.8086 8.16326 29.0479 5.52591 34.5837 4.83517C44.4115 3.57928 54.177 1.75824 65 0C51.8134 34.8509 29.6077 60.471 0 80Z" fill="#7CC5E5" />
+                    </svg>
 
+                    <div class="video h-100">
+                        <video src="" class="w-100 h-100" poster="<?= get_template_directory_uri() ?>/assets/images/background/business-needs.jpg" controls>
+                            <source src="movie.mp4" type="video/mp4">
+                            <source src="movie.ogg" type="video/ogg">
+                        </video>
+                    </div>
+
+
+                    <svg class="vector-front" xmlns="http://www.w3.org/2000/svg" width="386" height="185" viewBox="0 0 386 185" fill="none">
+                        <path d="M385.673 0.604606C374.582 77.4205 316.616 151.421 228.339 176.599C132.324 204.03 42.6337 157.414 0.763662 104.438C14.2935 100.525 27.4366 96.4556 40.7687 93.1862C42.9073 92.6376 46.2932 95.1886 48.3797 97.0913C75.5354 121.447 106.778 137.693 143.128 142.234C216.838 151.485 275.865 125.525 318.519 65.0548C328.262 51.282 333.113 34.0755 340.407 18.5546C341.627 15.8839 343.123 11.9987 345.251 11.3254C358.461 7.31289 371.865 4.16322 385.668 0.542251L385.673 0.604606Z" fill="#178BCB" />
+                    </svg>
                 </div>
             </div>
         </div>
